70cddfbef7e7c728fa5e478f0d68807108d86a7d
Anshuman Mishra <73525951+Obito2002@users.noreply.github.com>
2022-07-05 18:07:50 +0530

    Netflix clone v.1
    A practice clone of Netflix using React.js

---
+++ build/precache-manifest.bfd6a9b093a3023efd1d1fd72bfcf7e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eceafef1b843beadcbc04cbc7cd3b41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c6cc6eadbb587d1f1d7",
+    "url": "/static/css/main.e24c6389.chunk.css"
+  },
+  {
+    "revision": "924872b8d9ffe8fda2d6",
+    "url": "/static/js/2.0a917c6a.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.0a917c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6cc6eadbb587d1f1d7",
+    "url": "/static/js/main.6e8c7477.chunk.js"
+  },
+  {
+    "revision": "7dbe809d8ae504040e68",
+    "url": "/static/js/runtime-main.ebd86647.js"
+  }
+]);
